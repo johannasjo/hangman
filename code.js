7551422838e95d3c6c668dc559b46d94d2b653a3
Johanna Sjöstrand <johanna.sjoestrand@gmail.com>
2020-11-02 14:12:01 +0100

    #3 add corresponding lines for randomized word

--- code.js
+++ code.js
@@ -1,9 +1,20 @@
-function randomWords(arrayOfWords) {
-  let words = arrayOfWords;
-  let randomWord = Math.floor(Math.random() * words.length);
-  console.log(/* randomWord */ words[randomWord]);
+const words = ["pony", "monkey", "banana", "yellow", "brown", "poop", "dogs"];
+
+function randomWord(words) {
+  let randomWord = Math.floor(Math.random() * (words.length - 1));
+  /* console.log(randomWord, words[randomWord]); */
+  return words[randomWord];
 }
 
-console.log(
-  randomWords(["pony", "monkey", "banana", "yellow", "brown", "poop", "dogs"])
-);
+function makeEmptyLines() {
+  let word = randomWord(words);
+  console.log(word);
+  for (let letter of word) {
+    let lineElement = document.createElement("div");
+    lineElement.textContent = "_";
+    document.querySelector("#line-container").appendChild(lineElement);
+    console.log(lineElement);
+  }
+}
+
+console.log(makeEmptyLines());
